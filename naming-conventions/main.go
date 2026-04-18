0f64a65282c22f4f5e3cd2860df7d8c87e032508
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-04-18 09:59:17 +0530

    add naming convenstions

---
+++ naming-conventions/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Pascal Case
+	var FirstName = "Maria"
+	// Camel Case
+	var firstName = "Maria"
+	// Snake Case
+	var first_name = "Maria"
+
+	fmt.Println("Her name is: ", FirstName, firstName, first_name)
+}
