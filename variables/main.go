4b4c5326719d77314089faaa360ae3e4f62eb142
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-04-18 09:25:29 +0530

    add variables

---
+++ variables/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+const PORT = 3000
+
+var name = "Maria"
+var num = 29
+
+func main() {
+	name := "Devika"
+	fmt.Println("Her name is: ", name)
+	display()
+}
+
+func display(){
+fmt.Println("Her name is: ", name)	
+}
