ae524973c63ae655ffe916e038e5eca60bfbca12
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-04-23 10:56:40 +0530

    add for loop

---
+++ for-loop/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := [5]int{1, 2, 3, 4, 5}
+
+	for index, num := range nums {
+		fmt.Printf("Index is: %v\n Value is: %v\n", index, num)
+	}
+	for i := 0; i < 5; i++ {
+		fmt.Println("No is:", i)
+	}
+
+	for i := range 10 {
+		fmt.Println("Range is:", i)
+	}
+
+}
