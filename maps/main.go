54571cc600c2f6e361be8095520f0ecc347cb6f1
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-04-27 11:28:16 +0530

    add map data type

---
+++ maps/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var person map[string]string
+	// person := make(map[string]string)
+	// person["name"] = "Maria"
+	// person["age"] = "30"
+	// person["place"] = "Bridgen"
+	
+	fmt.Println("The user data is:", person)
+
+}
